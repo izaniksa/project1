68507d93ad60d52a588a575544686292b85f287c
Iza Niksa <izaniksa@MacBook-Air-Iza.local>
2020-02-11 23:16:50 +0100

    add init person form comp

---
+++ src/components/AddPersonForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="add-person-form">
+    <h1>Add person form</h1>
+    <div class="form-group">
+      <input v-model="name" placeholder="Name" />
+    </div>
+    <div class="form-group">
+      <input v-model="surname" placeholder="Surname" />
+    </div>
+    <button v-on:click="$emit('form-save', { name, surname })">Add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class AddPersonForm extends Vue {
+  @Prop() private msg!: string;
+  data() {
+    return {
+      name: "",
+      surname: ""
+    };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/AddPerson.vue
+++ src/views/AddPerson.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="add-person">
     <h1>This is an add person page</h1>
+    <AddPersonForm msg="Welcome to Your Vue.js App" v-on:form-save="onFormSave" />
   </div>
 </template>
+
+<script lang="ts">
+import AddPersonForm from "@/components/AddPersonForm.vue";
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {
+    AddPersonForm
+  },
+  methods: {
+    onFormSave: form => {
+      console.log(form);
+    }
+  }
+})
+export default class AddPerson extends Vue {}
+</script>
